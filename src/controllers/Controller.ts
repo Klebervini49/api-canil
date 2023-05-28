5bc5ae541d662f9477de41896f9d9ffa01c3d014
Klebervini49 <klebervini49@gmail.com>
2023-05-28 16:49:44 -0300

    atualizando mensagem home

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -2,7 +2,9 @@ import { Request, Response } from "express";
 import Pet from "../models/pet";
 
 export const Rotas = {
-  Home: (req: Request, res: Response) => res.send("Bem vindo a API do Canil!"),
+  Home: (req: Request, res: Response) => res.send({
+    message: "Bem vindo a API do Canil!"
+  }),
   HomeAll: (req: Request, res: Response) => res.send(Pet.listarTodos(res)),
   HomeAnimal: (req: Request, res: Response) => res.send(Pet.listarTipo(req.params.animal, res)),
   HomeSearch: (req: Request, res: Response) => res.send(Pet.listarRaca(req.params.pesquisa, res)),
